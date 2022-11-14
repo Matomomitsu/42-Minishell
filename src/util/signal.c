7827f1bde606250022496278f21e71a67f168faf
Rodrigo Lins <rlinsdev@gmail.com>
2022-11-14 09:57:23 -0300

    Signal - Completing doc

--- src/util/signal.c
+++ src/util/signal.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 17:25:21 by rlins             #+#    #+#             */
-/*   Updated: 2022/11/14 09:11:32 by rlins            ###   ########.fr       */
+/*   Updated: 2022/11/14 09:37:23 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ static void	reset_prompt(int signal);
 void	signals_handler()
 {
 	signal(SIGINT, reset_prompt);
+	// Ctrl+\ handler
 	signal(SIGQUIT, SIG_IGN);
 }
 
 /** TODO: Falta colocar a variável global com o status code correto aqui.
  * TODO: Signal deverá ser construído para HereDoc e / ou Child / parent proc.
- * @brief Will reset the prompt
+ * @brief Will reset the prompt. Ctrl+C handler action.
  * @param signal
  */
 static void	reset_prompt(int signal)
