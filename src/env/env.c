51f9ed4c6fe28c75deefa1d100ab87ed20f6eb7d
Rodrigo Lins <rlinsdev@gmail.com>
2022-11-17 09:15:10 -0300

    fixing return

--- src/env/env.c
+++ src/env/env.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 08:36:33 by rlins             #+#    #+#             */
-/*   Updated: 2022/11/17 09:13:30 by rlins            ###   ########.fr       */
+/*   Updated: 2022/11/17 09:14:44 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int	get_env_var_index(char **env, char *var)
 	index = 0;
 	tmp = ft_strjoin(var, "=");
 	if (!tmp)
-		return (NULL);
+		return (-1);
 	while (env[index])
 	{
 		if (ft_strncmp(tmp, env[index], ft_strlen(tmp) == 0))
