a96a16c282e7cef96b32eaaed0ec4edee315e6ec
Matomomitsu <matomomitsu@gmail.com>
2022-11-23 18:31:23 -0300

    editing exit_value

--- src/lexer/handle_lexer_errors.c
+++ src/lexer/handle_lexer_errors.c
@@ -12,29 +12,29 @@
 
 #include <minishell.h>
 
-void	lexer_errors(t_index_data *i_data, const char *s, t_data *data)
+void	lexer_errors(t_index_data *i_data, const char *s, t_commands *cmds)
 {
 	if (!s[i_data->i] && (s[i_data->i - 1] == '|' || s[i_data->i - 1] == '&'))
 	{
-		data->exit_value = 2;
+		cmds->exit_value = 2;
 		ft_putstr_fd("-minishell: syntax error not expecting newline\n", \
 		STDERR);
 	}
 	while (s[i_data->i] == '|' || s[i_data->i] == '&' || s[i_data->i] == ' ')
 	{
-		if ((s[i_data->i] == '|' || s[i_data->i] == '&') && data->exit_value \
+		if ((s[i_data->i] == '|' || s[i_data->i] == '&') && cmds->exit_value \
 		== 0)
 		{
-			data->exit_value = 2;
+			cmds->exit_value = 2;
 			ft_putstr_fd("-minishell: syntax error near unexpected token `", \
 			STDERR);
 			ft_putchar_fd(s[i_data->i], STDERR);
 			ft_putstr_fd("'\n", STDERR);
 		}
 		i_data->i++;
-		if (!s[i_data->i] && data->exit_value == 0)
+		if (!s[i_data->i] && cmds->exit_value == 0)
 		{
-			data->exit_value = 2;
+			cmds->exit_value = 2;
 			ft_putstr_fd("-minishell: syntax error not expecting newline\n", \
 			STDERR);
 		}
