bbb45ac268dc7a0543f2e85e6b7aae98f742f57d
Rodrigo Lins <rlinsdev@gmail.com>
2022-12-06 07:32:55 -0300

    IO - fixing problem when call with no redirection

--- src/redirection/io_handler.c
+++ src/redirection/io_handler.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 08:05:58 by rlins             #+#    #+#             */
-/*   Updated: 2022/12/05 09:28:25 by rlins            ###   ########.fr       */
+/*   Updated: 2022/12/06 07:00:33 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	redirect_io(t_io *io)
 {
+	if (!io)
+		return ;
 	io->std_in_bkp = dup(STDIN_FILENO);
 	if (io->std_in_bkp == -1)
 		error_msg_cmd("dup", "std-in-bkp", strerror(errno), false);
@@ -22,15 +24,28 @@ void	redirect_io(t_io *io)
 		error_msg_cmd("dup", "std-out-bkp", strerror(errno), false);
 	if (io->fd_in != -1)
 		if (dup2(io->fd_in, STDIN_FILENO) == -1)
-			error_msg_cmd("dup2", io->in_file_name, strerror(errno),
+			error_msg_cmd("dup2", io->in_file, strerror(errno),
 				EXIT_FAILURE);
 	if (io->fd_out != -1)
 		if (dup2(io->fd_out, STDOUT_FILENO) == -1)
-			error_msg_cmd("dup2", io->out_file_name, strerror(errno),
+			error_msg_cmd("dup2", io->out_file, strerror(errno),
 				EXIT_FAILURE);
 }
 
 void	restore_io(t_io *io)
 {
-
+	if (io->std_in_bkp != -1)
+	{
+		if (dup2(io->std_in_bkp, STDIN_FILENO) == -1)
+			return ;
+		close (io->std_in_bkp);
+		io->std_in_bkp = -1;
+	}
+	if (io->std_out_bkp != -1)
+	{
+		if (dup2(io->std_out_bkp, STDIN_FILENO) == -1)
+			return ;
+		close (io->std_out_bkp);
+		io->std_out_bkp = -1;
+	}
 }
