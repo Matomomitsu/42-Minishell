2f11e7efa573b5435e50a1f0cf6ea90965a55377
Matomomitsu <matomomitsu@gmail.com>
2022-12-07 23:23:11 -0300

    teste

--- src/execution/exec_child.c
+++ src/execution/exec_child.c
@@ -6,7 +6,7 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 03:06:05 by mtomomit          #+#    #+#             */
-/*   Updated: 2022/12/07 15:06:19 by mtomomit         ###   ########.fr       */
+/*   Updated: 2022/12/07 23:20:45 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,19 @@ int	exec_child(t_data *data, t_commands *cmds, int num_cmd)
 	while (num_cmd < cmds->num_cmds)
 	{
 		cmds->num_exec++;
-		cmds->pid[num_cmd] = fork();
-		if (cmds->pid[num_cmd] == -1)
-			return (error_msg_cmd("fork", NULL, strerror(errno),
-				EXIT_FAILURE));
-		else if (cmds->pid[num_cmd] == 0)
-			execute_cmd(data, cmds, num_cmd);
+		if (cmds->cmd[num_cmd].args[0])
+		{
+			cmds->pid[num_cmd] = fork();
+			if (cmds->pid[num_cmd] == -1)
+				return (error_msg_cmd("fork", NULL, strerror(errno),
+					EXIT_FAILURE));
+			else if (cmds->pid[num_cmd] == 0)
+				execute_cmd(data, cmds, num_cmd);
+			if (cmds->operators[num_cmd] == PIPE)
+				num_cmd++;
+			else
+				num_cmd = cmds->num_cmds;
+		}
 		if (cmds->operators[num_cmd] == PIPE)
 			num_cmd++;
 		else
